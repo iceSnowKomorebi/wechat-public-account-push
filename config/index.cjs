c7c510f74b72e8f5516d60d0f5ffb01c1ee309d1
iceSnowKomorebi <119877708+iceSnowKomorebi@users.noreply.github.com>
2022-12-06 00:45:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小雪宝老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oloWa5vPHDP7ch0fHdDBDrnTgFto',
+      id: 'oloWa5k5Kyp5l-pMH5nAttCp4aRs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'GGq1uO30u6lWY55guGLhXnMN0qY4jr_HmJ7CcJex_Rc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
